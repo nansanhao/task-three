4950e71be77f2fa4073937278a6d94f2850bcd44
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 19:38:15 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var sum=0;
+  if(typeof(str)=="string")
+  {
+   for (var i=0; i<str.length;i++)
+   {
+	   sum+=parseInt(str[i]);
+   }
+  }
+  return sum;
 };
